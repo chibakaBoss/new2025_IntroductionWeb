18e18d8ebececdf44fac932ab50f3bb68c3fc2d4
Халим <halim@Uranchimegs-MacBook-Pro.local>
2025-07-17 22:44:44 +0800

    test1

--- components/IntroScreen.tsx
+++ components/IntroScreen.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 
 const IntroScreen: React.FC = () => {
   const navigate = useNavigate();
-   console.log("-----")
+
   return (
     <div className="min-h-screen w-full bg-gradient-to-br from-zinc-900 via-zinc-800 to-gray-900 text-white px-6 py-12 flex flex-col items-center justify-center">
       <motion.div
@@ -28,8 +28,7 @@ const IntroScreen: React.FC = () => {
           </button>
           <button 
             onClick={() => navigate("/bye")}
-            className="px-6 py-3 border border-purple-500 text-purple-300 hover:bg-zinc-800 text-lg font-medium rounded-full transition-all"
-            >
+            className="px-6 py-3 border border-purple-500 text-purple-300 hover:bg-zinc-800 text-lg font-medium rounded-full transition-all">
             No thanks
           </button>
         </div>
